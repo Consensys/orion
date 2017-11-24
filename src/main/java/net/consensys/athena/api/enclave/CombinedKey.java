a431a82e261da53ee827b2b0ef83efbb3b5251db
Rob Dawson <robert@rojotek.com>
2017-11-24 11:18:14 +1000

    Added Javadoc around the CombinedKey interface.

--- src/main/java/net/consensys/athena/api/enclave/CombinedKey.java
+++ src/main/java/net/consensys/athena/api/enclave/CombinedKey.java
@@ -1,3 +1,14 @@
 package net.consensys.athena.api.enclave;
 
+/**
+ * A combined key for encryption. This interface contains no methods or constants. It merely serves
+ * to group (and provide type safety for) the combined key interface.
+ *
+ * <p>This takes inspiration from the java.security.PrivateKey/java.security.Key interface family.
+ *
+ * <p>Note: specialized combined key interfaces will extend this interface.
+ *
+ * @see java.security.Key
+ * @see java.security.PrivateKey
+ */
 public interface CombinedKey {}
