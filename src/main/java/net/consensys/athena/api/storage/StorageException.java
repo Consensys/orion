5907278c7047b65ffa7a172363633ab74ab7daf6
Rob Dawson <robert@rojotek.com>
2017-11-29 13:24:12 +1000

    Added in simple RuntimExceptions.

---
+++ src/main/java/net/consensys/athena/api/storage/StorageException.java
@@ -0,0 +1,21 @@
+package net.consensys.athena.api.storage;
+
+public class StorageException extends RuntimeException {
+
+  public StorageException(String message) {
+    super(message);
+  }
+
+  public StorageException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public StorageException(Throwable cause) {
+    super(cause);
+  }
+
+  protected StorageException(
+      String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+}
---
+++ src/test/java/net/consensys/athena/api/storage/StorageExceptionTest.java
@@ -0,0 +1,25 @@
+package net.consensys.athena.api.storage;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class StorageExceptionTest {
+
+  @Test
+  public void testImplementationOfRuntimeInterface() {
+    assertTrue(RuntimeException.class.isAssignableFrom(StorageException.class));
+  }
+
+  @Test
+  public void testConstruction() {
+    String message = "This is the cause";
+    StorageException exception = new StorageException(message);
+    assertEquals(message, exception.getMessage());
+    Throwable cause = new Throwable();
+    StorageException anotherException = new StorageException(message, cause);
+    assertEquals(message, anotherException.getMessage());
+    assertEquals(cause, anotherException.getCause());
+  }
+}
