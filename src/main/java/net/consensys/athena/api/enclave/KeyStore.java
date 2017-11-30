08bbe95814d254377d4650679d3f9044f673656f
Rob <robert@rojotek.com>
2017-11-30 17:09:03 +1000

    Updated the enclave API. (#12)
    Added KeyStore to encapsulate key management of private/public keys
    Made the Enclave API speak public keys, so the private key could be kept completely private with a real enclave.

---
+++ src/main/java/net/consensys/athena/api/enclave/KeyStore.java
@@ -0,0 +1,26 @@
+package net.consensys.athena.api.enclave;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+/**
+ * Interface for key store. Provides a mechanism for generating keys, and looking up a private key
+ * for a given public key. Typically used internally by an enclave to look up private keys.
+ */
+public interface KeyStore {
+
+  /**
+   * Lookup the private key for a given public key.
+   *
+   * @param publicKey PublicKey to get the private key for.
+   * @return Return the public key.
+   */
+  PrivateKey getPrivateKey(PublicKey publicKey);
+
+  /**
+   * Generate and store a new keypair, returning the public key for external use.
+   *
+   * @return Return the public key part of the key pair.
+   */
+  PublicKey generateKeyPair();
+}
