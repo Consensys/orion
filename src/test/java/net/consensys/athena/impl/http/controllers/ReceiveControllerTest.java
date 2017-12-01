cbd00b2a7a7b6072a6de8a3a4ac5016943a3ae93
Gautam Botrel <gautam.botrel@consensys.net>
2017-12-01 10:31:09 -0600

    added empty test class

---
+++ src/test/java/net/consensys/athena/impl/http/controllers/ReceiveControllerTest.java
@@ -0,0 +1,24 @@
+package net.consensys.athena.impl.http.controllers;
+
+import net.consensys.athena.api.enclave.Enclave;
+import net.consensys.athena.api.storage.Storage;
+import net.consensys.athena.api.storage.StorageKeyBuilder;
+import net.consensys.athena.impl.enclave.BouncyCastleEnclave;
+import net.consensys.athena.impl.http.server.Controller;
+import net.consensys.athena.impl.storage.Sha512_256StorageKeyBuilder;
+import net.consensys.athena.impl.storage.memory.MemoryStorage;
+
+import org.junit.Test;
+
+public class ReceiveControllerTest {
+  private final Enclave enclave = new BouncyCastleEnclave();
+  private final StorageKeyBuilder keyBuilder = new Sha512_256StorageKeyBuilder(enclave);
+  private final Storage storage = new MemoryStorage(keyBuilder);
+
+  private final Controller receiveController = new ReceiveController(enclave, storage);
+
+  @Test
+  public void testPayloadIsRetrieved() throws Exception {
+    // TODO
+  }
+}
