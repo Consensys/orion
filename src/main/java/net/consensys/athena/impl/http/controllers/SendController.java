2f0b09c111a2e0fbf6b90ef4173db318a5f1d725
Gautam Botrel <gautam.botrel@consensys.net>
2017-12-10 22:47:57 -0600

    fix merge, and added routerdb.wal.0 to gitignore

--- src/main/java/net/consensys/athena/impl/http/controllers/SendController.java
+++ src/main/java/net/consensys/athena/impl/http/controllers/SendController.java
@@ -8,6 +8,8 @@ import net.consensys.athena.impl.http.server.ContentType;
 import net.consensys.athena.impl.http.server.Controller;
 import net.consensys.athena.impl.http.server.Result;
 
+import java.security.PublicKey;
+
 import io.netty.handler.codec.http.FullHttpRequest;
 
 /** Send a base64 encoded payload to encrypt. */
@@ -24,6 +26,26 @@ public class SendController implements Controller {
 
   @Override
   public Result handle(FullHttpRequest request) {
+    // read request
+    // if request.from == null, use default node public key as "from"
+    // to = to + [nodeAlwaysSendTo] --> default pub key to always send to
+    // if to == null, set to to self public key
+    // TODO : that's a port from original Haskell code --> shouldn't we just validate the send request ? if to is set, we sent to "to", if not, we  return error ?
+    // convert payload from b64 to bytes
+    // encryptedPayload = enclave.encrypt(sendRequest.payload, from, to);
+    // toReturn = storage.store(encryptedPayload);
+    // if [to] is not only self, propagate payload to receipients
+    // for each t in [to], find the matching IP from public key, and call the /push API with the encryptedPayload
     return notImplemented(contentType);
+  }
+
+  private static class SendRequest {
+    String payload; // b64 encoded
+    PublicKey from;
+    PublicKey[] to;
+  }
+
+  private static class SendResponse {
+    String key; // b64 digest key result from encrypted payload storage operation
   }
 }
