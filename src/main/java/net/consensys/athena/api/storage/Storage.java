e683f63dec1906cd233807cd2daf8fba6776def8
Gautam Botrel <gautam.botrel@consensys.net>
2017-12-20 11:55:24 -0600

    added doc to Storage interface

--- src/main/java/net/consensys/athena/api/storage/Storage.java
+++ src/main/java/net/consensys/athena/api/storage/Storage.java
@@ -4,7 +4,15 @@ import java.util.Optional;
 
 public interface Storage<T> {
 
+  /**
+   * @param data
+   * @return the base64 encoded key, as an UTF-8 String
+   */
   String put(T data);
 
+  /**
+   * @param key should be base64 encoded UTF-8 string
+   * @return
+   */
   Optional<T> get(String key);
 }
