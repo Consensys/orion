332610c82fdbc9433494be975bd58f44acff1057
Gautam Botrel <gautam.botrel@consensys.net>
2018-01-08 16:18:54 -0600

    push controller is back

--- src/test/java/net/consensys/athena/impl/http/controllers/PushControllerTest.java
+++ src/test/java/net/consensys/athena/impl/http/controllers/PushControllerTest.java
@@ -1,44 +1,49 @@
 
 package net.consensys.athena.impl.http.controllers;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import net.consensys.athena.api.cmd.AthenaRoutes;
+import net.consensys.athena.api.enclave.EncryptedPayload;
+import net.consensys.athena.api.storage.Storage;
+import net.consensys.athena.impl.enclave.sodium.SodiumEncryptedPayload;
+import net.consensys.athena.impl.http.data.ContentType;
+
+import java.util.Optional;
+
+import okhttp3.MediaType;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.junit.Test;
+
 public class PushControllerTest extends ControllerTest {
 
-  //  @Test
-  //  public void testPayloadIsStored(TestContext context) {
-  //    final Async async = context.async();
-  //    // ref to storage
-  //    final Storage storage = routes.getStorage();
-  //
-  //    // build & serialize our payload
-  //    SodiumEncryptedPayload encryptedPayload = mockPayload();
-  //
-  //    // PUSH operation, sending an encrypted payload
-  //    Buffer toSend = Buffer.buffer(serializer.serialize(ContentType.CBOR, encryptedPayload));
-  //
-  //    vertx
-  //        .createHttpClient()
-  //        .post(
-  //            httpServerPort,
-  //            "localhost",
-  //            AthenaRoutes.PUSH,
-  //            response -> {
-  //              context.assertEquals(200, response.statusCode());
-  //              response.handler(
-  //                  body -> {
-  //                    // PUSH should return payload digest
-  //                    String digest = body.toString();
-  //                    context.assertTrue(digest.length() > 0);
-  //
-  //                    // we should be able to read that from storage
-  //                    storage.get(digest);
-  //                    Optional<EncryptedPayload> data = storage.get(digest);
-  //
-  //                    context.assertTrue(data.isPresent());
-  //                    context.assertEquals(encryptedPayload, data.get());
-  //
-  //                    async.complete();
-  //                  });
-  //            })
-  //        .end(toSend);
-  //  }
+  @Test
+  public void testPayloadIsStored() throws Exception {
+    // ref to storage
+    final Storage storage = routes.getStorage();
+
+    // build & serialize our payload
+    SodiumEncryptedPayload encryptedPayload = mockPayload();
+
+    // PUSH operation, sending an encrypted payload
+    RequestBody body =
+        RequestBody.create(
+            MediaType.parse(ContentType.CBOR.httpHeaderValue),
+            serializer.serialize(ContentType.CBOR, encryptedPayload));
+
+    Request request = new Request.Builder().post(body).url(baseUrl + AthenaRoutes.PUSH).build();
+
+    try (Response resp = httpClient.newCall(request).execute()) {
+      assertEquals(200, resp.code());
+      String digest = resp.body().string();
+      assertTrue(digest.length() > 0);
+      // we should be able to read that from storage
+      Optional<EncryptedPayload> data = storage.get(digest);
+      assertTrue(data.isPresent());
+      assertEquals(encryptedPayload, data.get());
+    }
+  }
 }
