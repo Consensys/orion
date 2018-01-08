1f60c2c72fbdcde15cf15692e647b4d29871c6ff
Rob Dawson <robert@rojotek.com>
2018-01-09 06:13:07 +1000

    Added bouncy castle provider to hasher.

--- src/main/java/net/consensys/athena/impl/enclave/Hasher.java
+++ src/main/java/net/consensys/athena/impl/enclave/Hasher.java
@@ -4,8 +4,14 @@ import net.consensys.athena.api.enclave.HashAlgorithm;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
 
 public class Hasher {
+  //TODO consider interface/implementation split
+  static {
+    Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+  }
+
   public byte[] digest(HashAlgorithm algorithm, byte[] input) {
     MessageDigest digest;
     try {
