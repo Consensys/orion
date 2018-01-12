2158a793b1ff9e1f5094a454d5dfb8a7beb9f9cd
Gautam Botrel <gautam.botrel@consensys.net>
2018-01-12 12:18:26 -0600

    added acceptance test with 2 nodes

--- src/acceptance/java/net/consensys/athena/acceptance/SendReceiveTest.java
+++ src/acceptance/java/net/consensys/athena/acceptance/SendReceiveTest.java
@@ -18,59 +18,37 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import org.junit.Before;
 import org.junit.Test;
 
 public class SendReceiveTest {
 
-  private final String configFilePath =
-      this.getClass().getClassLoader().getResource("singlenode.conf").getPath();
+  private static final byte[] originalPayload = "a wonderful transaction".getBytes();
 
-  private static final String baseUrl = "http://127.0.0.1:9001";
+  private final String singleNodeConfig =
+      getClass().getClassLoader().getResource("singlenode.conf").getPath();
+  private final String node1Config =
+      getClass().getClassLoader().getResource("node1.conf").getPath();
+  private final String node2Config =
+      getClass().getClassLoader().getResource("node2.conf").getPath();
+
+  private static final String singleNodeBaseUrl = "http://127.0.0.1:9001";
+  private static final String node1BaseUrl = "http://127.0.0.1:9002";
+  private static final String node2BaseUrl = "http://127.0.0.1:9003";
+
   private static final OkHttpClient httpClient = new OkHttpClient();
-  private static final Request upRequest =
-      new Request.Builder().get().url(baseUrl + AthenaRoutes.UPCHECK).build();
   private static final Serializer serializer = new Serializer();
 
   private static final String pk1b64 = "A1aVtMxLCUHmBVHXoZzzBgPbW/wj5axDpW9X8l91SGo=";
   private static final String pk2b64 = "Ko2bVqD+nNlNYL5EE7y3IdOnviftjiizpjRt+HTuFBs=";
 
-  @Before
-  public void setUp() throws Exception {
-    // setup a single node with 2 public keys
-    // generate public / private keys
-    Athena athena = new Athena();
-    //    athena.run(new String[] {"-g", "key1"});
-    //    athena.run(new String[] {"-g", "key2"});
-    athena.run(new String[] {configFilePath});
-  }
-
   @Test
   public void testSingleNode() throws Exception {
-    // ensure the node is awake
-    Response upResponse = httpClient.newCall(upRequest).execute();
-    assertEquals(200, upResponse.code());
-    assertEquals("I'm up!", upResponse.body().string());
+    // setup a single node with 2 public keys
+    Athena athena = new Athena();
+    athena.run(new String[] {singleNodeConfig});
 
-    byte[] originalPayload =
-        ("pragma solidity ^0.4.17;\n"
-                + "\n"
-                + "contract SimpleStorage {\n"
-                + "  uint public storedData;\n"
-                + "\n"
-                + "  function SimpleStorage(uint initVal) public {\n"
-                + "    storedData = initVal;\n"
-                + "  }\n"
-                + "\n"
-                + "  function set(uint x) public {\n"
-                + "    storedData = x;\n"
-                + "  }\n"
-                + "\n"
-                + "  function get() constant public returns (uint retVal) {\n"
-                + "    return storedData;\n"
-                + "  }\n"
-                + "}")
-            .getBytes();
+    // ensure the node is awake
+    ensureNodeIsAwake(singleNodeBaseUrl);
 
     // send something to the node (from pk1 to pk2)
     SendRequest sendRequest =
@@ -80,7 +58,7 @@ public class SendReceiveTest {
             MediaType.parse(JSON.httpHeaderValue), serializer.serialize(JSON, sendRequest));
 
     Request httpSendRequest =
-        new Request.Builder().post(sendBody).url(baseUrl + AthenaRoutes.SEND).build();
+        new Request.Builder().post(sendBody).url(singleNodeBaseUrl + AthenaRoutes.SEND).build();
     Response httpSendResponse = httpClient.newCall(httpSendRequest).execute();
     assertEquals(200, httpSendResponse.code());
 
@@ -95,7 +73,10 @@ public class SendReceiveTest {
             MediaType.parse(JSON.httpHeaderValue), serializer.serialize(JSON, receiveRequest));
 
     Request httpReceiveRequest =
-        new Request.Builder().post(receiveBody).url(baseUrl + AthenaRoutes.RECIEVE).build();
+        new Request.Builder()
+            .post(receiveBody)
+            .url(singleNodeBaseUrl + AthenaRoutes.RECIEVE)
+            .build();
     Response httpReceiveResponse = httpClient.newCall(httpReceiveRequest).execute();
     assertEquals(200, httpReceiveResponse.code());
 
@@ -104,5 +85,61 @@ public class SendReceiveTest {
         serializer.deserialize(JSON, ReceiveResponse.class, httpReceiveResponse.body().bytes());
     byte[] receivedPayload = Base64.decode(receiveResponse.payload);
     assertArrayEquals(originalPayload, receivedPayload);
+  }
+
+  @Test
+  public void testTwoNodes() throws Exception {
+    // setup our 2 nodes
+    Athena node1 = new Athena();
+    node1.run(new String[] {node1Config});
+    ensureNodeIsAwake(node1BaseUrl);
+
+    Athena node2 = new Athena();
+    node2.run(new String[] {node2Config});
+    ensureNodeIsAwake(node2BaseUrl);
+
+    // ensure network discovery ran on node 1
+    Thread.sleep(1000);
+
+    // send a transaction from node1 to node2
+    SendRequest sendRequest =
+        new SendRequest(Base64.encode(originalPayload), pk1b64, new String[] {pk2b64});
+    RequestBody sendBody =
+        RequestBody.create(
+            MediaType.parse(JSON.httpHeaderValue), serializer.serialize(JSON, sendRequest));
+
+    Request httpSendRequest =
+        new Request.Builder().post(sendBody).url(node1BaseUrl + AthenaRoutes.SEND).build();
+    Response httpSendResponse = httpClient.newCall(httpSendRequest).execute();
+    System.out.println(httpSendResponse.body().string());
+    assertEquals(200, httpSendResponse.code());
+    // deserialize the response
+    SendResponse sendResponse =
+        serializer.deserialize(JSON, SendResponse.class, httpSendResponse.body().bytes());
+
+    // call receive on the node 2
+    ReceiveRequest receiveRequest = new ReceiveRequest(sendResponse.key, pk2b64);
+    RequestBody receiveBody =
+        RequestBody.create(
+            MediaType.parse(JSON.httpHeaderValue), serializer.serialize(JSON, receiveRequest));
+
+    Request httpReceiveRequest =
+        new Request.Builder().post(receiveBody).url(node2BaseUrl + AthenaRoutes.RECIEVE).build();
+    Response httpReceiveResponse = httpClient.newCall(httpReceiveRequest).execute();
+    assertEquals(200, httpReceiveResponse.code());
+
+    // deserialize the response
+    ReceiveResponse receiveResponse =
+        serializer.deserialize(JSON, ReceiveResponse.class, httpReceiveResponse.body().bytes());
+    byte[] receivedPayload = Base64.decode(receiveResponse.payload);
+
+    assertArrayEquals(originalPayload, receivedPayload);
+  }
+
+  private void ensureNodeIsAwake(String baseUrl) throws Exception {
+    Request upRequest = new Request.Builder().get().url(baseUrl + AthenaRoutes.UPCHECK).build();
+    Response upResponse = httpClient.newCall(upRequest).execute();
+    assertEquals(200, upResponse.code());
+    assertEquals("I'm up!", upResponse.body().string());
   }
 }
