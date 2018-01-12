594477411d454cbe03540f2bf7318fcf92f5366b
Gautam Botrel <gautam.botrel@consensys.net>
2018-01-12 10:28:52 -0600

    fix graddle

---
+++ src/acceptance/java/net/consensys/athena/acceptance/SimpleTest.java
@@ -0,0 +1,13 @@
+package net.consensys.athena.acceptance;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+  @Test
+  public void testSomethingSimple() {
+    assertFalse(true);
+  }
+}
--- src/acceptanceTest/java/net/consensys/athena/SingleNodeTest.java
+++
@@ -1,9 +0,0 @@
-package net.consensys.athena;
-
-import org.junit.Test;
-
-public class SingleNodeTest {
-
-  @Test
-  public void testSomething() {}
-}
