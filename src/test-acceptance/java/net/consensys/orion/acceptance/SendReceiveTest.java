384db7c9e13a98b965d5c5adc0552349faa5e64d
cjhare <CjHare@users.noreply.github.com>
2018-02-28 16:12:49 +1000

    UT covering simple case

--- src/test-acceptance/java/net/consensys/orion/acceptance/SendReceiveTest.java
+++ src/test-acceptance/java/net/consensys/orion/acceptance/SendReceiveTest.java
@@ -22,178 +22,199 @@ import junit.framework.AssertionFailedError;
 import okhttp3.HttpUrl;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SendReceiveTest {
 
-  private static final byte[] originalPayload = "a wonderful transaction".getBytes();
+	private static final byte[] originalPayload = "a wonderful transaction".getBytes();
 
-  private static String singleNodeBaseUrl;
-  private static String node1BaseUrl;
-  private static String node2BaseUrl;
+	private static String singleNodeBaseUrl;
+	private static String node1BaseUrl;
+	private static String node2BaseUrl;
 
-  private static Config configSingleNode;
-  private static Config configNode1;
-  private static Config configNode2;
+	private static Config configSingleNode;
+	private static Config configNode1;
+	private static Config configNode2;
 
-  private static final String PK_1_B_64 = "A1aVtMxLCUHmBVHXoZzzBgPbW/wj5axDpW9X8l91SGo=";
-  private static final String PK_2_B_64 = "Ko2bVqD+nNlNYL5EE7y3IdOnviftjiizpjRt+HTuFBs=";
+	private static final String PK_1_B_64 = "A1aVtMxLCUHmBVHXoZzzBgPbW/wj5axDpW9X8l91SGo=";
+	private static final String PK_2_B_64 = "Ko2bVqD+nNlNYL5EE7y3IdOnviftjiizpjRt+HTuFBs=";
 
-  private static final String HOST_NAME = "127.0.0.1";
+	private static final String HOST_NAME = "127.0.0.1";
 
-  @AfterClass
-  public static void tearDown() throws Exception {
-    Path rootPath = Paths.get("database");
-    Files.walk(rootPath, FileVisitOption.FOLLOW_LINKS)
-        .sorted(Comparator.reverseOrder())
-        .map(Path::toFile)
-        .forEach(File::delete);
-  }
+	@AfterClass
+	public static void tearDown() throws Exception {
 
-  @BeforeClass
-  public static void setUp() throws Exception {
-    // generate base urls
-    int node1Port = getFreePort();
-    int node2Port = getFreePort();
-    int singleNodePort = getFreePort();
-    node1BaseUrl =
-        new HttpUrl.Builder().scheme("http").host(HOST_NAME).port(node1Port).build().toString();
-    node2BaseUrl =
-        new HttpUrl.Builder().scheme("http").host(HOST_NAME).port(node2Port).build().toString();
-    singleNodeBaseUrl =
-        new HttpUrl.Builder()
-            .scheme("http")
-            .host(HOST_NAME)
-            .port(singleNodePort)
-            .build()
-            .toString();
+		Path rootPath = Paths.get("database");
+		Files.walk(rootPath, FileVisitOption.FOLLOW_LINKS).sorted(Comparator.reverseOrder()).map(Path::toFile)
+		        .forEach(File::delete);
+	}
 
-    // Single node config
-    String confString =
-        new StringBuilder()
-            .append("url = \"")
-            .append(singleNodeBaseUrl)
-            .append("\"\nport = ")
-            .append(singleNodePort)
-            .append("\nstorage = \"leveldb:database/node0\"")
-            .append("\nothernodes = [\"")
-            .append(singleNodeBaseUrl)
-            .append(
-                "\"]\n"
-                    + "publickeys = [\"src/test-acceptance/resources/key1.pub\", \"src/test-acceptance/resources/key2.pub\"]\n"
-                    + "privatekeys = [\"src/test-acceptance/resources/key1.key\", \"src/test-acceptance/resources/key2.key\"]")
-            .toString();
+	@BeforeClass
+	public static void setUp() throws Exception {
 
-    configSingleNode =
-        new TomlConfigBuilder()
-            .build(new ByteArrayInputStream(confString.getBytes(StandardCharsets.UTF_8.name())));
+		// generate base urls
+		int node1Port = getFreePort();
+		int node2Port = getFreePort();
+		int singleNodePort = getFreePort();
+		node1BaseUrl = new HttpUrl.Builder().scheme("http").host(HOST_NAME).port(node1Port).build().toString();
+		node2BaseUrl = new HttpUrl.Builder().scheme("http").host(HOST_NAME).port(node2Port).build().toString();
+		singleNodeBaseUrl = new HttpUrl.Builder().scheme("http").host(HOST_NAME).port(singleNodePort).build()
+		        .toString();
 
-    // node1 config
-    confString =
-        new StringBuilder()
-            .append("url = \"")
-            .append(node1BaseUrl)
-            .append("\"\nport = ")
-            .append(node1Port)
-            .append("\nstorage = \"leveldb:database/node1\"")
-            .append("\nothernodes = [\"")
-            .append(node2BaseUrl)
-            .append(
-                "\"]\n"
-                    + "publickeys = [\"src/test-acceptance/resources/key1.pub\"]\n"
-                    + "privatekeys = [\"src/test-acceptance/resources/key1.key\"]")
-            .toString();
+		// Single node config
+		String confString = new StringBuilder().append("url = \"").append(singleNodeBaseUrl).append("\"\nport = ")
+		        .append(singleNodePort).append("\nstorage = \"leveldb:database/node0\"").append("\nothernodes = [\"")
+		        .append(singleNodeBaseUrl)
+		        .append("\"]\n"
+		                + "publickeys = [\"src/test-acceptance/resources/key1.pub\", \"src/test-acceptance/resources/key2.pub\"]\n"
+		                + "privatekeys = [\"src/test-acceptance/resources/key1.key\", \"src/test-acceptance/resources/key2.key\"]")
+		        .toString();
 
-    configNode1 =
-        new TomlConfigBuilder()
-            .build(new ByteArrayInputStream(confString.getBytes(StandardCharsets.UTF_8.name())));
+		configSingleNode = new TomlConfigBuilder()
+		        .build(new ByteArrayInputStream(confString.getBytes(StandardCharsets.UTF_8.name())));
 
-    // node2 config
-    confString =
-        new StringBuilder()
-            .append("url = \"")
-            .append(node2BaseUrl)
-            .append("\"\nport = ")
-            .append(node2Port)
-            .append("\nstorage = \"leveldb:database/node2\"")
-            .append("\nothernodes = [\"")
-            .append(node1BaseUrl)
-            .append(
-                "\"]\n"
-                    + "publickeys = [\"src/test-acceptance/resources/key2.pub\"]\n"
-                    + "privatekeys = [\"src/test-acceptance/resources/key2.key\"]")
-            .toString();
+		// node1 config
+		confString = new StringBuilder().append("url = \"").append(node1BaseUrl).append("\"\nport = ").append(node1Port)
+		        .append("\nstorage = \"leveldb:database/node1\"").append("\nothernodes = [\"").append(node2BaseUrl)
+		        .append("\"]\n" + "publickeys = [\"src/test-acceptance/resources/key1.pub\"]\n"
+		                + "privatekeys = [\"src/test-acceptance/resources/key1.key\"]")
+		        .toString();
 
-    configNode2 =
-        new TomlConfigBuilder()
-            .build(new ByteArrayInputStream(confString.getBytes(StandardCharsets.UTF_8.name())));
-  }
+		configNode1 = new TomlConfigBuilder()
+		        .build(new ByteArrayInputStream(confString.getBytes(StandardCharsets.UTF_8.name())));
 
-  private static int getFreePort() throws Exception {
-    ServerSocket socket = new ServerSocket(0);
-    int toReturn = socket.getLocalPort();
-    socket.close();
-    return toReturn;
-  }
+		// node2 config
+		confString = new StringBuilder().append("url = \"").append(node2BaseUrl).append("\"\nport = ").append(node2Port)
+		        .append("\nstorage = \"leveldb:database/node2\"").append("\nothernodes = [\"").append(node1BaseUrl)
+		        .append("\"]\n" + "publickeys = [\"src/test-acceptance/resources/key2.pub\"]\n"
+		                + "privatekeys = [\"src/test-acceptance/resources/key2.key\"]")
+		        .toString();
 
-  @Test
-  public void testSingleNode() throws Exception {
-    // setup a single node with 2 public keys
-    Orion orion = new Orion();
-    orion.run(configSingleNode);
-    OrionClient orionClient = new OrionClient(singleNodeBaseUrl);
+		configNode2 = new TomlConfigBuilder()
+		        .build(new ByteArrayInputStream(confString.getBytes(StandardCharsets.UTF_8.name())));
+	}
 
-    // ensure the node is awake
-    assertTrue(orionClient.upCheck());
+	private static int getFreePort() throws Exception {
 
-    // send something to the node (from pk1 to pk2)
-    String digest =
-        orionClient
-            .send(originalPayload, PK_1_B_64, new String[] {PK_2_B_64})
-            .orElseThrow(AssertionFailedError::new);
+		ServerSocket socket = new ServerSocket(0);
+		int toReturn = socket.getLocalPort();
+		socket.close();
+		return toReturn;
+	}
 
-    // call receive on the node
-    byte[] receivedPayload =
-        orionClient.receive(digest, PK_2_B_64).orElseThrow(AssertionFailedError::new);
+	@Test
+	public void testSingleNode() throws Exception {
 
-    // ensure we retrieved what we originally sent.
-    assertArrayEquals(originalPayload, receivedPayload);
+		// setup a single node with 2 public keys
+		Orion orion = new Orion();
+		orion.run(configSingleNode);
+		OrionClient orionClient = new OrionClient(singleNodeBaseUrl);
 
-    // stop
-    orion.stop();
-  }
+		// ensure the node is awake
+		assertTrue(orionClient.upCheck());
 
-  @Test
-  public void testTwoNodes() throws Exception {
-    // setup our 2 nodes
-    Orion orionLauncher1 = new Orion();
-    orionLauncher1.run(configNode1);
-    OrionClient node1 = new OrionClient(node1BaseUrl);
-    assertTrue(node1.upCheck());
+		// send something to the node (from pk1 to pk2)
+		String digest = orionClient.send(originalPayload, PK_1_B_64, new String[] { PK_2_B_64 })
+		        .orElseThrow(AssertionFailedError::new);
 
-    Orion orionLauncher2 = new Orion();
-    orionLauncher2.run(configNode2);
-    OrionClient node2 = new OrionClient(node2BaseUrl);
-    assertTrue(node2.upCheck());
+		// call receive on the node
+		byte[] receivedPayload = orionClient.receive(digest, PK_2_B_64).orElseThrow(AssertionFailedError::new);
 
-    // ensure network discovery ran on node 1
-    Thread.sleep(1000);
+		// ensure we retrieved what we originally sent.
+		assertArrayEquals(originalPayload, receivedPayload);
 
-    // send a transaction from node1 to node2
-    String digest =
-        node1
-            .send(originalPayload, PK_1_B_64, new String[] {PK_2_B_64})
-            .orElseThrow(AssertionFailedError::new);
+		// stop
+		orion.stop();
+	}
 
-    // call receive on the node 2
-    byte[] receivedPayload =
-        node2.receive(digest, PK_2_B_64).orElseThrow(AssertionFailedError::new);
+	/**
+	 * Single node sends and receives using just one key.
+	 */
+	@Test
+	public void singleNodeKeyIdentity() throws Exception {
 
-    // ensure we retrieved what we originally sent.
-    assertArrayEquals(originalPayload, receivedPayload);
+		// setup a single node with 2 public keys
+		Orion orion = new Orion();
+		orion.run(configSingleNode);
+		OrionClient orionClient = new OrionClient(singleNodeBaseUrl);
 
-    // stop
-    orionLauncher1.stop();
-    orionLauncher2.stop();
-  }
+		// ensure the node is awake
+		assertTrue(orionClient.upCheck());
+
+		// send something to the node (from pk2 to pk2)
+		String digest = orionClient.send(originalPayload, PK_2_B_64, new String[] { PK_2_B_64 })
+		        .orElseThrow(AssertionFailedError::new);
+
+		// call receive on the node
+		byte[] receivedPayload = orionClient.receive(digest, PK_2_B_64).orElseThrow(AssertionFailedError::new);
+
+		// ensure we retrieved what we originally sent.
+		assertArrayEquals(originalPayload, receivedPayload);
+
+		// stop
+		orion.stop();
+	}
+
+	@Ignore
+	/**
+	 * Single node sends and receives using different keys.
+	 * Ensures the sender key can view their transaction when not also the recipient key.
+	 */
+	@Test
+	public void singleNodeOneKeyToAnother() throws Exception {
+
+		// setup a single node with 2 public keys
+		Orion orion = new Orion();
+		orion.run(configSingleNode);
+		OrionClient orionClient = new OrionClient(singleNodeBaseUrl);
+
+		// ensure the node is awake
+		assertTrue(orionClient.upCheck());
+
+		// send something to the node (from pk1 to pk2)
+		String digest = orionClient.send(originalPayload, PK_1_B_64, new String[] { PK_2_B_64 })
+		        .orElseThrow(AssertionFailedError::new);
+
+		// call receive on the node
+		byte[] receivedPayload = orionClient.receive(digest, PK_1_B_64).orElseThrow(AssertionFailedError::new);
+
+		// ensure we retrieved what we originally sent.
+		assertArrayEquals(originalPayload, receivedPayload);
+
+		// stop
+		orion.stop();
+	}
+
+	@Test
+	public void testTwoNodes() throws Exception {
+
+		// setup our 2 nodes
+		Orion orionLauncher1 = new Orion();
+		orionLauncher1.run(configNode1);
+		OrionClient node1 = new OrionClient(node1BaseUrl);
+		assertTrue(node1.upCheck());
+
+		Orion orionLauncher2 = new Orion();
+		orionLauncher2.run(configNode2);
+		OrionClient node2 = new OrionClient(node2BaseUrl);
+		assertTrue(node2.upCheck());
+
+		// ensure network discovery ran on node 1
+		Thread.sleep(1000);
+
+		// send a transaction from node1 to node2
+		String digest = node1.send(originalPayload, PK_1_B_64, new String[] { PK_2_B_64 })
+		        .orElseThrow(AssertionFailedError::new);
+
+		// call receive on the node 2
+		byte[] receivedPayload = node2.receive(digest, PK_2_B_64).orElseThrow(AssertionFailedError::new);
+
+		// ensure we retrieved what we originally sent.
+		assertArrayEquals(originalPayload, receivedPayload);
+
+		// stop
+		orionLauncher1.stop();
+		orionLauncher2.stop();
+	}
 }
