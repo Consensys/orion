b00012e7fa04d6d1c8ec2e2088ee9636dfb293ba
cjhare <CjHare@users.noreply.github.com>
2018-03-12 13:42:28 +1000

    Re-arranging error handler code

--- src/main/java/net/consensys/orion/impl/http/server/vertx/HttpErrorHandler.java
+++ src/main/java/net/consensys/orion/impl/http/server/vertx/HttpErrorHandler.java
@@ -4,10 +4,12 @@ import net.consensys.orion.impl.http.server.HttpContentType;
 import net.consensys.orion.impl.http.server.HttpError;
 import net.consensys.orion.impl.utils.Serializer;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Route;
 import io.vertx.ext.web.RoutingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,25 +17,19 @@ import org.apache.logging.log4j.Logger;
 public class HttpErrorHandler implements Handler<RoutingContext> {
   private static final Logger log = LogManager.getLogger();
 
+  private final Serializer serializer;
+
   public HttpErrorHandler(Serializer serializer) {
     this.serializer = serializer;
   }
 
-  private final Serializer serializer;
-
   @Override
   public void handle(RoutingContext failureContext) {
-    int statusCode = failureContext.statusCode();
-    statusCode = statusCode < 0 ? 500 : statusCode;
+    final int statusCode = statusCode(failureContext);
+    final HttpServerResponse response = failureContext.response().setStatusCode(statusCode);
 
-    HttpServerResponse response = failureContext.response().setStatusCode(statusCode);
-
-    if (failureContext.failure() != null) {
-      HttpError httpError = new HttpError(failureContext.failure().getMessage());
-      Buffer buffer = Buffer.buffer(serializer.serialize(HttpContentType.JSON, httpError));
-
-      log.error(failureContext.currentRoute().getPath() + " failed " + httpError);
-      log.error(failureContext.failure().getStackTrace());
+    if (hasError(failureContext)) {
+      final Buffer buffer = errorJson(failureContext.failure(), failureContext.currentRoute());
 
       response
           .putHeader(HttpHeaders.CONTENT_TYPE, HttpContentType.JSON.httpHeaderValue)
@@ -41,5 +37,29 @@ public class HttpErrorHandler implements Handler<RoutingContext> {
     } else {
       response.end();
     }
+  }
+
+  private Buffer errorJson(final Throwable failure, final Route failureRoute) {
+    final HttpError httpError = new HttpError(failure.getMessage());
+    final Buffer buffer = Buffer.buffer(serializer.serialize(HttpContentType.JSON, httpError));
+
+    log.error(failureRoute.getPath() + " failed " + httpError);
+    log.error(failure.getStackTrace());
+
+    return buffer;
+  }
+
+  /**
+   * Status code may not have been set (left as a negative number), in which case assume server side
+   * issue.
+   */
+  private int statusCode(RoutingContext failureContext) {
+    return failureContext.statusCode() < 0
+        ? HttpResponseStatus.INTERNAL_SERVER_ERROR.code()
+        : failureContext.statusCode();
+  }
+
+  private boolean hasError(RoutingContext failureContext) {
+    return failureContext.failure() != null;
   }
 }
