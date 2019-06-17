363be7cbc5003d861f8b05492eb78ca5212eebc7
Puneetha <puneetha17@gmail.com>
2019-06-17 17:01:25 +0100

    Rethrow the Exception in SendHandler in after getting privacy group.

--- src/main/java/net/consensys/orion/http/handler/send/SendHandler.java
+++ src/main/java/net/consensys/orion/http/handler/send/SendHandler.java
@@ -32,6 +32,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
@@ -129,6 +130,9 @@ public class SendHandler implements Handler<RoutingContext> {
               .fail(new OrionException(OrionErrorCode.ENCLAVE_PRIVACY_GROUP_MISSING, "privacy group not found"));
           return result;
         }
+      }).exceptionally(e -> {
+        handleFailure(routingContext, e);
+        return Optional.empty();
       });
     }
   }
