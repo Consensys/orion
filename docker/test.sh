8c6cea9f4f98914e4622271d67da051ed458629f
Usman Saleem <usman@usmans.info>
2019-11-21 21:59:48 +1000

    Fixing goss docker tests and updating circleci

--- docker/test.sh
+++ docker/test.sh
@@ -6,28 +6,50 @@ export GOSS_FILES_STRATEGY=cp
 DOCKER_IMAGE=$1
 DOCKER_FILE="${2:-$PWD/Dockerfile}"
 
+cleanup() {
+    set +e
+    echo "Cleaning temporary data directory"
+    rm -rf "$tmp_dir"
+}
+
 i=0
 
-## Generate public/private key pair
-tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+tmp_dir=$(mktemp -d /tmp/tmp.XXXXXXXXXX)
+chmod 777 "$tmp_dir"
+trap 'ret=$?;cleanup;exit $ret' EXIT
+echo "Temp Data Directory: ${tmp_dir}"
 
-GOSS_FILES_PATH=tests/00 \
-yes 123456 | bash tests/dgoss run $DOCKER_IMAGE \
---mount type=bind,source=$tmp_dir,target=/data -g /data/nodeKey
-> ./reports/00.xml || i=`expr $i + 1`
+mkdir -p ./reports
+
+## Generate public/private key pair
+# pipe password 123456 to orion
+yes 123456 | docker run -i --rm --mount type=bind,source=$tmp_dir,target=/data $DOCKER_IMAGE -g /data/nodeKey
+
+# Create password file
+cat << EOF > $tmp_dir/passwordFile
+123456
+EOF
+
+## Create orion configuration file
+cat << EOF > $tmp_dir/orion.conf
+nodeurl = "http://127.0.0.1:8080/"
+nodeport = 8080
+nodenetworkinterface = "0.0.0.0"
+clienturl = "http://127.0.0.1:8888/"
+clientport = 8888
+clientnetworkinterface = "0.0.0.0"
+publickeys = ["/data/nodeKey.pub"]
+privatekeys = ["/data/nodeKey.key"]
+passwords = "/data/passwordFile"
+workdir = "/data"
+tls = "off"
+EOF
+
 ls -l $tmp_dir/
-# fail fast if we dont pass static checks
-if [[ $i != 0 ]]; then exit $i; fi
 
 # Test for normal startup with ports opened
-# we test that things listen on the right interface/port, not what interface the advertise
-# hence we dont set p2p-host=0.0.0.0 because this sets what its advertising to devp2p; the important piece is that it defaults to listening on all interfaces
-#GOSS_FILES_PATH=tests/01 \
-#bash tests/dgoss run $DOCKER_IMAGE \
-#--network=dev \
-#--rpc-http-enabled \
-#--rpc-ws-enabled \
-#--graphql-http-enabled \
-#> ./reports/01.xml || i=`expr $i + 1`
+GOSS_FILES_PATH=tests/01 \
+bash tests/dgoss run --mount type=bind,source=$tmp_dir,target=/data $DOCKER_IMAGE /data/orion.conf \
+> ./reports/01.xml || i=`expr $i + 1`
 
 exit $i
