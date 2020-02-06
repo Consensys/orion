4b163a69752f311a47259c99060573c2ef756575
Jason Frame <jasonwframe@gmail.com>
2020-02-06 13:16:53 +1000

    Unit tests for legacy privacy group id generation (#321)

---
+++ src/test/java/net/consensys/orion/enclave/sodium/SodiumEnclavePrivacyGroupIdTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package net.consensys.orion.enclave.sodium;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import net.consensys.orion.enclave.PrivacyGroupPayload.Type;
+
+import java.security.Security;
+import java.util.Arrays;
+
+import org.apache.tuweni.bytes.Bytes;
+import org.apache.tuweni.crypto.sodium.Box.PublicKey;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SodiumEnclavePrivacyGroupIdTest {
+  private static final String ENCLAVE_PUBLIC_KEY_1 = "negmDcN2P4ODpqn/6WkJ02zT/0w0bjhGpkZ8UP6vARk=";
+  private static final String ENCLAVE_PUBLIC_KEY_2 = "g59BmTeJIn7HIcnq8VQWgyh/pDbvbt2eyP0Ii60aDDw=";
+  private static final String ENCLAVE_PUBLIC_KEY_3 = "6fg8q5rWMBoAT2oIiU3tYJbk4b7oAr7dxaaVY7TeM3U=";
+
+  // This is the expected generated legacy privacy group id for a privacy group containing the enclave
+  // public keys enclave_public_key_1, enclave_public_key_2 and enclave_public_key_3
+  private static final String EXPECTED_LEGACY_PRIVACY_GROUP_ID = "/xzRjCLioUBkm5LYuzll61GXyrD5x7bvXzQk/ovJA/4=";
+
+  private final MemoryKeyStore keyStore = new MemoryKeyStore();
+  private SodiumEnclave enclave;
+
+  static {
+    Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+  }
+
+  @BeforeEach
+  void setUp() {
+    enclave = new SodiumEnclave(keyStore);
+  }
+
+  @Test
+  public void generatesSameLegacyPrivacyGroupIdForDuplicateValues() {
+    assertThat(
+        legacyPrivacyGroupId(ENCLAVE_PUBLIC_KEY_2, ENCLAVE_PUBLIC_KEY_1, ENCLAVE_PUBLIC_KEY_3, ENCLAVE_PUBLIC_KEY_1))
+            .isEqualTo(EXPECTED_LEGACY_PRIVACY_GROUP_ID);
+    assertThat(
+        legacyPrivacyGroupId(ENCLAVE_PUBLIC_KEY_2, ENCLAVE_PUBLIC_KEY_1, ENCLAVE_PUBLIC_KEY_1, ENCLAVE_PUBLIC_KEY_3))
+            .isEqualTo(EXPECTED_LEGACY_PRIVACY_GROUP_ID);
+    assertThat(legacyPrivacyGroupId(ENCLAVE_PUBLIC_KEY_2, ENCLAVE_PUBLIC_KEY_1, ENCLAVE_PUBLIC_KEY_3))
+        .isEqualTo(EXPECTED_LEGACY_PRIVACY_GROUP_ID);
+  }
+
+  @Test
+  public void generatesSameLegacyPrivacyGroupIdForPrivateForInDifferentOrders() {
+    assertThat(legacyPrivacyGroupId(ENCLAVE_PUBLIC_KEY_1, ENCLAVE_PUBLIC_KEY_2, ENCLAVE_PUBLIC_KEY_3))
+        .isEqualTo(EXPECTED_LEGACY_PRIVACY_GROUP_ID);
+    assertThat(legacyPrivacyGroupId(ENCLAVE_PUBLIC_KEY_1, ENCLAVE_PUBLIC_KEY_3, ENCLAVE_PUBLIC_KEY_2))
+        .isEqualTo(EXPECTED_LEGACY_PRIVACY_GROUP_ID);
+    assertThat(legacyPrivacyGroupId(ENCLAVE_PUBLIC_KEY_2, ENCLAVE_PUBLIC_KEY_1, ENCLAVE_PUBLIC_KEY_3))
+        .isEqualTo(EXPECTED_LEGACY_PRIVACY_GROUP_ID);
+    assertThat(legacyPrivacyGroupId(ENCLAVE_PUBLIC_KEY_2, ENCLAVE_PUBLIC_KEY_3, ENCLAVE_PUBLIC_KEY_1))
+        .isEqualTo(EXPECTED_LEGACY_PRIVACY_GROUP_ID);
+    assertThat(legacyPrivacyGroupId(ENCLAVE_PUBLIC_KEY_3, ENCLAVE_PUBLIC_KEY_1, ENCLAVE_PUBLIC_KEY_2))
+        .isEqualTo(EXPECTED_LEGACY_PRIVACY_GROUP_ID);
+    assertThat(legacyPrivacyGroupId(ENCLAVE_PUBLIC_KEY_3, ENCLAVE_PUBLIC_KEY_2, ENCLAVE_PUBLIC_KEY_1))
+        .isEqualTo(EXPECTED_LEGACY_PRIVACY_GROUP_ID);
+  }
+
+  private String legacyPrivacyGroupId(final String... recipientsAndSender) {
+    final PublicKey[] publicKeys =
+        Arrays.stream(recipientsAndSender).map(Bytes::fromBase64String).map(PublicKey::fromBytes).toArray(
+            PublicKey[]::new);
+    return Bytes.of(enclave.generatePrivacyGroupId(publicKeys, null, Type.LEGACY)).toBase64String();
+  }
+}
