a2dc6b0f2e7e67506a7255eae240844f6e3e9549
Antoine Toulme <antoine@lunar-ocean.com>
2020-02-17 21:27:45 -0800

    add test for two persistent nodes reusing the same store

---
+++ src/test/java/net/consensys/orion/network/PersistentNetworkNodesTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package net.consensys.orion.network;
+
+import static org.apache.tuweni.crypto.sodium.Box.KeyPair.random;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import net.consensys.orion.config.Config;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.tuweni.crypto.sodium.Box;
+import org.apache.tuweni.kv.MapKeyValueStore;
+import org.junit.jupiter.api.Test;
+
+class PersistentNetworkNodesTest {
+
+  @Test
+  void testTwoInstancesSameStore() {
+    Config config = Config.load("tls='off'");
+    MapKeyValueStore<Box.PublicKey, URI> store = MapKeyValueStore.open(new ConcurrentHashMap<>());
+    PersistentNetworkNodes nodes1 = new PersistentNetworkNodes(config, new Box.PublicKey[0], store);
+    PersistentNetworkNodes nodes2 = new PersistentNetworkNodes(config, new Box.PublicKey[0], store);
+
+    Box.PublicKey pk = random().publicKey();
+    boolean changed = nodes1.addNode(Collections.singletonMap(pk, URI.create("http://example:com:56666")).entrySet());
+    assertTrue(changed);
+
+    Iterator<Map.Entry<Box.PublicKey, URI>> iter = nodes2.nodePKs().iterator();
+    Map.Entry<Box.PublicKey, URI> entry = iter.next();
+    assertEquals(pk, entry.getKey());
+    assertFalse(iter.hasNext());
+  }
+
+  @Test
+  void testTwoInstancesInSequence() {
+    Config config = Config.load("tls='off'");
+    MapKeyValueStore<Box.PublicKey, URI> store = MapKeyValueStore.open(new ConcurrentHashMap<>());
+    PersistentNetworkNodes nodes1 = new PersistentNetworkNodes(config, new Box.PublicKey[0], store);
+
+    Box.PublicKey pk = random().publicKey();
+    boolean changed = nodes1.addNode(Collections.singletonMap(pk, URI.create("http://example:com:56666")).entrySet());
+    assertTrue(changed);
+
+    PersistentNetworkNodes nodes2 = new PersistentNetworkNodes(config, new Box.PublicKey[0], store);
+    Iterator<Map.Entry<Box.PublicKey, URI>> iter = nodes2.nodePKs().iterator();
+    Map.Entry<Box.PublicKey, URI> entry = iter.next();
+    assertEquals(pk, entry.getKey());
+    assertFalse(iter.hasNext());
+  }
+}
