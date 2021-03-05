6f6d63a3e85d7ecb841df037584f9c6b7c436a2a
Sally MacFarlane <sally.macfarlane@consensys.net>
2021-03-05 11:26:38 +1000

    Cloudsmith republish (#410)
    * use unified maven repo
    * republish only if SNAPSHOT
    
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- scripts/cloudsmith-upload.sh
+++ scripts/cloudsmith-upload.sh
@@ -5,6 +5,11 @@ ORION_VERSION=${1:?Must specify orion version}
 TAR_DIST=${2:?Must specify path to tar distribution}
 ZIP_DIST=${3:?Must specify path to zip distribution}
 
+REPUBLISH=""
+if [[ $VERSION == *"SNAPSHOT"* ]]; then
+  REPUBLISH="--republish"
+fi
+
 ENV_DIR=./build/tmp/cloudsmith-env
 if [[ -d ${ENV_DIR} ]] ; then
     source ${ENV_DIR}/bin/activate
@@ -17,7 +22,7 @@ python3 -m pip install --upgrade cloudsmith-cli
 
 if [ -z ${CLOUDSMITH_USER+x} ]; then echo "CLOUDSMITH_USER is unset"; else echo "CLOUDSMITH_USER is set to '$CLOUDSMITH_USER'"; fi
 
-cloudsmith push raw consensys/orion $TAR_DIST --name 'orion.tar.gz' --version "${ORION_VERSION}" --summary "Orion ${ORION_VERSION} binary distribution" --description "Binary distribution of Orion ${ORION_VERSION}." --content-type 'application/tar+gzip'
-cloudsmith push raw consensys/orion $ZIP_DIST --name 'orion.zip' --version "${ORION_VERSION}" --summary "Orion ${ORION_VERSION} binary distribution" --description "Binary distribution of Orion ${ORION_VERSION}." --content-type 'application/zip'
+cloudsmith push raw consensys/orion $TAR_DIST ${REPUBLISH} --name 'orion.tar.gz' --version "${ORION_VERSION}" --summary "Orion ${ORION_VERSION} binary distribution" --description "Binary distribution of Orion ${ORION_VERSION}." --content-type 'application/tar+gzip'
+cloudsmith push raw consensys/orion $ZIP_DIST ${REPUBLISH} --name 'orion.zip' --version "${ORION_VERSION}" --summary "Orion ${ORION_VERSION} binary distribution" --description "Binary distribution of Orion ${ORION_VERSION}." --content-type 'application/zip'
 
 echo "files uploaded to cloudsmith for version ${ORION_VERSION}"
